086b91e204959c7276c809425f93aab89c8995b7
Vladimir Ignatev <ya.na.pochte@gmail.com>
2025-10-20 05:23:22 +0800

    temporarily deleted ads modules

--- ads/src/main/java/com/google/ads/core/AdRevenueListenerFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.google.ads.core
-
-import com.google.android.gms.ads.OnPaidEventListener
-
-open class AdRevenueListenerFactory {
-    open fun createListenerWith(adUnitId: String, adObject: Any): OnPaidEventListener {
-        return OnPaidEventListener { }
-    }
-}
