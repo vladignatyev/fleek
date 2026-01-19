966d1fbfe26b7ff6ba7d8941f21d409a28ba83e1
Vladimir Ignatev <ya.na.pochte@gmail.com>
2026-01-19 15:47:44 +0800

    deferred paid events and paid events filtering

--- ads/src/main/java/com/google/fleekads/attribution/Tenjin.kt
+++ ads/src/main/java/com/google/fleekads/attribution/Tenjin.kt
@@ -4,17 +4,27 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.util.Log
+import com.google.android.gms.ads.AdValue
 import com.google.android.gms.ads.OnPaidEventListener
 import com.google.fleekads.core.AdRevenueListenerFactory
 import com.tenjin.android.TenjinSDK
 import org.json.JSONObject
 
 
+// TODO: Make a general class independent of Tenjin
 class TenjinAttributionWithAdRevenue(
     private val activity: Context, private val sdkKey: String
 ) : AdRevenueListenerFactory() {
     private var sdkInstance: TenjinSDK = TenjinSDK.getInstance(activity, sdkKey)!!
         get() = field
+
+    var reportImmediately: Boolean = false
+
+    var reportPrecise: Boolean = true
+    var reportEstimated: Boolean = false
+    var reportPublisherProvided: Boolean = false
+    var reportUnknown: Boolean = false
+    var reportZero: Boolean = false
 
     override fun createListenerWith(adUnitId: String, adObject: Any): OnPaidEventListener {
         Log.d(ILRD_TAG, "Creating ILRD listener for `${adUnitId}`")
@@ -23,28 +33,98 @@ class TenjinAttributionWithAdRevenue(
             throw IllegalStateException("adUnitId shouldn't be empty! ${adObject.toString()}")
         }
 
-        return OnPaidEventListener { adValue ->
-            // Get the ad unit ID
-            val adUnitId = adUnitId
+        return createDeferredPaidEventListener(adUnitId)
+    }
 
-            // Extract the impression-level ad revenue data
-            val valueMicros = adValue.valueMicros
-            val currencyCode = adValue.currencyCode
-            val precisionType = adValue.precisionType
-
+    data class PaidEvent(
+        val adUnitId: String,
+        val adValueMicros: Long,
+        val currencyCode: String,
+        val precisionType: Int
+    ) {
+        fun toJSON(): JSONObject {
             val json = JSONObject()
             json.put("ad_unit_id", adUnitId)
             json.put("currency_code", currencyCode)
-            json.put("value_micros", valueMicros)
+            json.put("value_micros", adValueMicros)
             json.put("precision_type", precisionType)
-
-            sdkInstance.eventAdImpressionAdMob(json)
-
-            Log.d(
-                ILRD_TAG,
-                "Ad Revenue Reported:\n\tAd Unit: $adUnitId\n\tValue:${adValue.valueMicros}\n\tCurrency:${adValue.currencyCode}\n\tPrecision:${adValue.precisionType}"
-            )
+            return json
         }
+
+        companion object {
+            fun fromAdValue(adUnitId: String, adValue: AdValue): PaidEvent {
+                // Extract the impression-level ad revenue data
+                val valueMicros = adValue.valueMicros
+                val currencyCode = adValue.currencyCode
+                val precisionType = adValue.precisionType
+
+                return PaidEvent(
+                    adUnitId = adUnitId,
+                    adValueMicros = valueMicros,
+                    currencyCode = currencyCode,
+                    precisionType = precisionType,
+                )
+            }
+        }
+    }
+
+
+    val deferredPaidEvents = ArrayList<PaidEvent>()
+
+    private fun createDeferredPaidEventListener(adUnitId: String): OnPaidEventListener =
+        OnPaidEventListener { adValue ->
+            if (reportImmediately) {
+                reportAdRevenue(event = PaidEvent.fromAdValue(adUnitId, adValue))
+            } else {
+                storeAdRevenue(event = PaidEvent.fromAdValue(adUnitId, adValue))
+            }
+        }
+
+    private fun reportAdRevenue(event: PaidEvent) {
+        sdkInstance.eventAdImpressionAdMob(event.toJSON())
+
+        Log.d(
+            ILRD_TAG,
+            "Ad Revenue Reported:\n\tAd Unit: $event.adUnitId\n\tValue:${event.adValueMicros}\n\tCurrency:${event.currencyCode}\n\tPrecision:${event.precisionType}"
+        )
+    }
+
+    private fun storeAdRevenue(event: PaidEvent) {
+        deferredPaidEvents.add(event)
+    }
+
+    val predicate: (PaidEvent) -> Boolean = { event ->
+        (reportZero || event.adValueMicros > 0) && (
+                (reportPrecise && event.precisionType == AdValue.PrecisionType.PRECISE)
+                        || (reportEstimated && event.precisionType == AdValue.PrecisionType.ESTIMATED)
+                        || (reportPublisherProvided && event.precisionType == AdValue.PrecisionType.PUBLISHER_PROVIDED)
+                        || (reportUnknown && event.precisionType == AdValue.PrecisionType.UNKNOWN)
+                )
+    }
+
+    fun reportDefferedPaidEvents(): List<PaidEvent> {
+        Log.d(
+            ILRD_TAG,
+            "*** Reporting deferred paid events. Totally ${deferredPaidEvents.count()} events caught.***"
+        )
+
+        Log.d(
+            ILRD_TAG, "${deferredPaidEvents.count(predicate)} events will be reported."
+        )
+
+        val toBeReported = ArrayList<PaidEvent>(deferredPaidEvents.filter(predicate))
+
+        toBeReported.map { event ->
+            reportAdRevenue(event)
+        }
+
+        deferredPaidEvents.clear()
+
+        Log.d(
+            ILRD_TAG, "*** Deferred events reported and queue has been cleared ***"
+        )
+
+        return toBeReported
     }
 
     fun provideGdpr() {
@@ -79,8 +159,7 @@ class TenjinAttributionWithAdRevenue(
         fun getInstance(): TenjinAttributionWithAdRevenue {
             if (instance == null) {
                 throw IllegalStateException(
-                    "You should create TenjinAttributionWithAdRevenue instance " +
-                            "in onStart() of your Activity before calling super onStart(savedInstanceState)."
+                    "You should create TenjinAttributionWithAdRevenue instance " + "in onStart() of your Activity before calling super onStart(savedInstanceState)."
                 )
             }
             return instance!!
@@ -89,3 +168,4 @@ class TenjinAttributionWithAdRevenue(
         const val ILRD_TAG = "ILRD"
     }
 }
+
